b914aad4fdb65e7992b7d2023b71a5f6d37bb5fe
Shiva <stark.code.css@gmail.com>
2025-11-15 01:02:22 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js";
-import checklistRoutes from "./routes/checklistRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import errorHandler from "./middleware/errorHandler.js";
+import connectDB from "./src/config/db.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import checklistRoutes from "./src/routes/checklistRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import errorHandler from "./src/middleware/errorHandler.js";
 
 dotenv.config();
 const app = express();
