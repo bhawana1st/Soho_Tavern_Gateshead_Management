2f1ed2372412090a595c96ca350a91fc1efd4aae
Shiva <stark.code.css@gmail.com>
2025-11-15 00:54:26 +0530

    changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./src/config/db.js";
-import authRoutes from "./src/routes/authRoutes.js";
-import checklistRoutes from "./src/routes/checklistRoutes.js";
-import userRoutes from "./src/routes/userRoutes.js";
-import errorHandler from "./src/middleware/errorHandler.js";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import checklistRoutes from "./routes/checklistRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 const app = express();
@@ -37,4 +37,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
-export default app;
